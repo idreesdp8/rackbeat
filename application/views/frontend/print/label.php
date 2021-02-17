346eaae863fd0922bea20fab5e47e625e31dbad0
HamzaZafarBhatti <hamza0952454@gmail.com>
2021-02-17 11:54:47 +0500

    print font change

--- application/views/frontend/print/label.php
+++ application/views/frontend/print/label.php
@@ -24,14 +24,14 @@
 
 <body>
     <div class="print-page" style="padding: 1.25rem;">
-        <p style="font-size: 16px; font-family: <?php echo $design->font_style ?>"><?php echo $product->number ?></p>
+        <p style="font-size: 12px; font-family: <?php echo $design->font_style ?>"><?php echo $product->number ?></p>
         <p style="font-size: 20px; font-weight: bold; font-family: <?php echo $design->font_style ?>"><?php echo $product->name ?></p>
         <div style="display: flex; justify-content: space-between;">
-            <p style="font-size: 14px; font-family: <?php echo $design->font_style ?>">Qty: <?php echo $qty ?></p>
-            <p style="font-size: 14px; font-family: <?php echo $design->font_style ?>">Lot #: <?php echo date('d/m/Y', strtotime($product->created_at)) ?></p>
+            <p style="font-size: 10px; font-family: <?php echo $design->font_style ?>">Qty: <?php echo $qty ?></p>
+            <p style="font-size: 10px; font-family: <?php echo $design->font_style ?>">Lot #: <?php echo date('d/m/Y', strtotime($product->created_at)) ?></p>
         </div>
         <img id="preview_img" src="<?php echo barcode_url() . $barcode_image ?>" alt="" style="width: 100%;">
-        <p style="font-family: <?php echo $design->font_style ?>">Group: <?php echo $product->group ? $product->group->name : 'NA' ?></p>
+        <p style="font-size: 10px; font-family: <?php echo $design->font_style ?>">Group: <?php echo $product->group ? $product->group->name : 'NA' ?></p>
     </div>
 
     <script>
